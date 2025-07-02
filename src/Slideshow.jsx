aa50730c743b4f42fff782b77ec976c4bb760ba0
Nathan Perlman <145302069+SwordPuffin@users.noreply.github.com>
2025-07-02 14:33:04 -0400

    Update Slideshow.jsx

--- src/Slideshow.jsx
+++ src/Slideshow.jsx
@@ -1,23 +1,22 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css'; 
+import './Slideshow.css'
 
 function Slideshow() {
   return (
-    <div style={{ width: '75%', marginTop: '20px', marginBottom: '20px', margin: 'auto', backgroundSize: 'cover'}}>
-      <Splide 
-        options={{
-          type     : 'loop',
-          perPage   : 1,
-          autoplay  : true,
-          interval  : 6000,
-          pagination: true,
-          arrows   : true,
-        }}
-      >
-        <SplideSlide><img style={{width: '75%', height: '85%', display: 'flex', margin: 'auto', borderRadius: '20px'}} src="./src/assets/carousel_photo_1.jpg" alt="1" /></SplideSlide>
-        <SplideSlide><img style={{borderRadius: '20px'}} src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/11/cropped-pxl_20240220_175528049.jpg" alt="2" /></SplideSlide>
-        <SplideSlide><img style={{borderRadius: '20px'}} src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/11/cropped-pxl_20240220_175528049.jpg" alt="3" /></SplideSlide>
-      </Splide>
+    <div className='carousel'>
+      <div className="slider">
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_0739.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3749.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3194.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3497.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/p1090241.jpg" alt="image"/>
+      </div>
+      <div aria-hidden className='slider'>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_0739.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3749.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3194.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/img_3497.jpg" alt="image"/>
+        <img src="https://martingrovemodelun.wordpress.com/wp-content/uploads/2024/12/p1090241.jpg" alt="image"/>
+      </div>
     </div>
   );
 }
